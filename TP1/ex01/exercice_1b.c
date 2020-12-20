421c685ce0a4482963303b56e4cc693738dc655e
MouadIzlgue <mouadizelgue@gmail.com>
2020-12-20 17:57:32 -0500

    ex1b

--- TP1/ex01/exercice_1b.c
+++ TP1/ex01/exercice_1b.c
@@ -9,10 +9,24 @@ float *remontee(float A[N][N], float B[N], int n)
   ** you may consider to create a dynamically allocated array and return a pointer to it. \
   ** https://gribblelab.org/CBootCamp/7_Memory_Stack_vs_Heap.html
   */
-
+	float *x;
+	x=malloc(sizeof(float)*n);
   /******Implement the solution here******/
   /***************************************/
-  
+ 
+
+	x[n-1]=B[n-1]/A[n-1][n-1];
+	for(int i=n-2;i>=0;i--)
+	{
+		float s=0;
+		for(int j=i+1;j<n;j++)
+		{
+			s=s+A[i][j]*x[j];
+		}
+		x[i]=(B[i]-s)/A[i][i];
+
+
+ 	}  
   /***************************************/
   return x;
 }
